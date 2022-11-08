59f0ac9e80a4446f2d0438a0d5a2ae56add20ba4
Boris Pavlov <borzpavlov@gmail.com>
2022-11-08 10:12:09 +0500

    Добавил метод в консольное меню

--- Test_Task/ProgramMenu.cs
+++ Test_Task/ProgramMenu.cs
@@ -14,20 +14,64 @@ namespace Test_Task {
         /// </summary>
         public void StartMenu() {
             ShowMenu();
+            StartWork();
         }
 
         /// <summary>
         /// Вывод меню программы
         /// </summary>
         private void ShowMenu() {
-            Console.WriteLine("Введите номер действия:\n");
-            Console.WriteLine("1. Загрузка дерева тэгов из XML файла.");
-            Console.WriteLine("2. Выгрузка дерева тэгов в XML файл.");
-            Console.WriteLine("3. Вывод списка тэгов.");
-            Console.WriteLine("4. Удаление тэга.");
-            Console.WriteLine("5. Добавление нового тэга.");
-            Console.WriteLine("6. Переименование тэга.");
-            Console.WriteLine("7. Вывести меню программы");
+            Console.WriteLine("Доступные действия:");
+            Console.WriteLine("  1. Загрузка дерева тэгов из XML файла.");
+            Console.WriteLine("  2. Выгрузка дерева тэгов в XML файл.");
+            Console.WriteLine("  3. Вывод списка тэгов.");
+            Console.WriteLine("  4. Удаление тэга.");
+            Console.WriteLine("  5. Добавление нового тэга.");
+            Console.WriteLine("  6. Переименование тэга.");
+            Console.WriteLine("  7. Вывести меню программы.");
+            Console.WriteLine("  8. Завершить программу.");
+        }
+
+        /// <summary>
+        /// Запуск работы программы
+        /// </summary>
+        private void StartWork() {
+            while (true) {
+                Console.WriteLine("\n\nВведите номер действия:");
+                string action = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (action) {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        ShowMenu();
+                        break;
+                    case "8":
+                        StopWork();
+                        break;
+                    default:
+                        Console.WriteLine("Недопустимое действие!");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Завершение работы программы
+        /// </summary>
+        private void StopWork() {
+            System.Environment.Exit(1);
         }
     }
 }
